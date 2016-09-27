cb106c3f326b4372a180ebe4ff5fd6b5299e0500
Kevin Moens <kmoens@lakeco.com>
2016-09-27 11:39:55 -0500

    Separated out Code from form to a separate class

--- SytelineDevTools/FormMetaDataExtractor.UI/frmOpen.vb
+++ SytelineDevTools/FormMetaDataExtractor.UI/frmOpen.vb
@@ -8,56 +8,23 @@
             MessageBox.Show("Select Form to Open", "Open Form", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             Exit Sub
         End If
-        OpenSelectedItem()
-    End Sub
-    Private Sub OpenSelectedItem()
-        Dim oFormType = CType(CType(lbForms.SelectedItem, FormItem).Form, Type)
-        Dim oForm = oFormType.GetConstructor({}).Invoke({})
-        CType(oForm, Form).MdiParent = frmMain
-        CType(oForm, Form).Show()
+        FormItem.OpenItem(lbForms.SelectedItem)
         Me.DialogResult = DialogResult.OK
     End Sub
-
     Private Sub frmOpen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         RefreshList()
     End Sub
     Private Sub RefreshList()
         lbForms.DataSource = Nothing
-        lbForms.DataSource = GetFormMenuItems()
+        lbForms.DataSource = FormItem.GetFormMenuItems(txtFilter.Text)
         lbForms.DisplayMember = "Description"
     End Sub
-    Private Function GetFormMenuItems() As List(Of FormItem)
-        Dim lFormItems As New List(Of FormItem)
-        For Each oType In Me.GetType().Assembly.GetTypes
-            Dim oInterface As Type = Nothing
-            Dim oFormItem As New FormItem
-            If oType.BaseType Is GetType(Form) Then
-                Try
-                    Dim oFormMenuItem As FormMenuItem = Nothing
-                    For Each oAttr As Attribute In oType.GetCustomAttributes(True)
-                        If oAttr.ToString = "FormMetaDataExtractor.UI.FormMenuItem" Then
-                            oFormMenuItem = CType(oAttr, FormMenuItem)
-                            Exit For
-                        End If
-                    Next
-                    If oFormMenuItem IsNot Nothing Then
-                        oFormItem.Description = oFormMenuItem.Description
-                        If txtFilter.Text = "" OrElse oFormItem.Description.ToUpper Like "*" & txtFilter.Text.ToUpper & "*" Then
-                            oFormItem.Form = oType
-                            lFormItems.Add(oFormItem)
-                        End If
-                    End If
-                Catch
-                End Try
-            End If
-        Next
-        Return lFormItems.OrderBy(Function(i) i.Description)
-    End Function
 
 
     Private Sub lbForms_DoubleClick(sender As Object, e As EventArgs) Handles lbForms.DoubleClick
         If lbForms.SelectedItem IsNot Nothing Then
-            OpenSelectedItem()
+            FormItem.OpenItem(lbForms.SelectedItem)
+            Me.DialogResult = DialogResult.OK
         End If
     End Sub
 
@@ -65,11 +32,17 @@
     Private Sub lbForms_KeyPress(sender As Object, e As KeyPressEventArgs) Handles lbForms.KeyPress
         If lbForms.SelectedItem IsNot Nothing Then
             If e.KeyChar = Chr(13) Then
-                OpenSelectedItem()
+                FormItem.OpenItem(lbForms.SelectedItem)
+                Me.DialogResult = DialogResult.OK
             End If
         End If
     End Sub
 End Class
+''' <summary>
+''' Class For Display in List Box
+''' Used to Get List of Forms
+''' Used to open instance of form
+''' </summary>
 Public Class FormItem
 
     Private _Description As String
@@ -90,4 +63,40 @@ Public Class FormItem
             _Form = value
         End Set
     End Property
+
+    Public Shared Function GetFormMenuItems(InFilter As String) As List(Of FormItem)
+        Dim lFormItems As New List(Of FormItem)
+        For Each oType In System.Reflection.Assembly.GetExecutingAssembly.GetTypes
+            Dim oInterface As Type = Nothing
+            Dim oFormItem As New FormItem
+            If oType.BaseType Is GetType(Form) Then
+                Try
+                    Dim oFormMenuItem As FormMenuItem = Nothing
+                    For Each oAttr As Attribute In oType.GetCustomAttributes(True)
+                        If oAttr.ToString = "FormMetaDataExtractor.UI.FormMenuItem" Then
+                            oFormMenuItem = CType(oAttr, FormMenuItem)
+                            Exit For
+                        End If
+                    Next
+                    If oFormMenuItem IsNot Nothing Then
+                        oFormItem.Description = oFormMenuItem.Description
+                        If InFilter = "" OrElse oFormItem.Description.ToUpper Like "*" & InFilter.ToUpper & "*" Then
+                            oFormItem.Form = oType
+                            lFormItems.Add(oFormItem)
+                        End If
+                    End If
+                Catch
+                End Try
+            End If
+        Next
+        Return lFormItems.OrderBy(Function(i) i.Description)
+    End Function
+
+    Public Shared Sub OpenItem(InItem As FormItem)
+        Dim oFormType = CType(InItem.Form, Type)
+        Dim oForm = oFormType.GetConstructor({}).Invoke({})
+        CType(oForm, Form).MdiParent = frmMain
+        CType(oForm, Form).Show()
+    End Sub
+
 End Class
