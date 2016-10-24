c212de8574162628d470907dadb66684485162db
Kevin Moens <kmoens@lakeco.com>
2016-10-24 00:00:44 -0500

    Early Commit...
    Working on creating a data scripting method that works with IDOs.
    Working on loading default data for SyteLineDevTools.  (new IDOs needed to function with form scripting)

---
+++ SytelineDevTools/SyteLineDevTools/IDOInvoke.vb
@@ -0,0 +1,49 @@
+﻿Public Class IDOInvoke
+    Public Sub New(InIDOName As String, InMethod As String, InParameters As List(Of String))
+        _IDOName = InIDOName
+        _Method = InMethod
+        _Parameters = InParameters
+    End Sub
+    Private _IDOName As String
+    Public Property IDOName() As String
+        Get
+            Return _IDOName
+        End Get
+        Set(ByVal value As String)
+            _IDOName = value
+        End Set
+    End Property
+    Private _Method As String
+    Public Property Method() As String
+        Get
+            Return _Method
+        End Get
+        Set(ByVal value As String)
+            _Method = value
+        End Set
+    End Property
+    Private _Parameters As List(Of String)
+    Public Property Parameters() As List(Of String)
+        Get
+            Return _Parameters
+        End Get
+        Set(ByVal value As List(Of String))
+            _Parameters = value
+        End Set
+    End Property
+    Public Sub ExportJson(writer As Newtonsoft.Json.JsonTextWriter, InvokeType As String)
+        writer.WritePropertyName(InvokeType)
+        writer.WriteStartObject()
+        writer.WritePropertyName("IDOName")
+        writer.WriteValue(IDOName)
+        writer.WritePropertyName("Method")
+        writer.WriteValue(Method)
+        writer.WritePropertyName("Parameters")
+        writer.WriteStartArray()
+        For Each p In Parameters
+            writer.WriteValue(p)
+        Next
+        writer.WriteEndArray()
+        writer.WriteEndObject()
+    End Sub
+End Class
